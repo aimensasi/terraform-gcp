11cfa890a426c8260caeafcc901271a0b2f81594
aimensasi <aimensasi@icloud.com>
2024-09-18 08:30:02 +0800

    change name

--- src/modules/cloud_build/main.tf
+++ src/modules/cloud_build/main.tf
@@ -42,7 +42,7 @@ resource "google_cloudbuild_trigger" "build_trigger" {
 
 
 resource "google_cloudbuild_trigger" "build_trigger_v2" {
-  name = "gcp-starter-build-trigger"
+  name = "gcp-starter-build-trigger-v2"
   location = var.region
 
   repository_event_config {
